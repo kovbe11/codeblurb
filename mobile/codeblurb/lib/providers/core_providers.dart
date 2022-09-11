79bb319e03b312bf29761e8d6f33172baaac2387
Czeglédi Levente <czegledi.levente@wadjet.hu>
2022-09-11 11:40:09 +0200

    fix analyzer issues

--- mobile/codeblurb/lib/providers/core_providers.dart
+++ mobile/codeblurb/lib/providers/core_providers.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_remote_config/firebase_remote_config.dart';
-import 'package:riverpod/riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final remoteConfigProvider = Provider<FirebaseRemoteConfig>(
