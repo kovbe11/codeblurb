4220840e893fdb9789f33db2581719d157aaec74
Czeglédi Levente <czegledi.levente@wadjet.hu>
2022-09-11 12:43:16 +0200

    App basics (#1)
    * add basic launch configs
    
    * add basic routing, l10n and structure
    
    * integrate basics with firebase
    
    * fix analyzer issues
    
    * delete untargeted platform libs
    
    * Update .gitignore
    
    * register FirebaseAnalytics properly
    
    * fix format issues

---
+++ mobile/codeblurb/lib/utils/logger.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Logger extends ProviderObserver {
+  const Logger();
+
+  @override
+  void didUpdateProvider(
+    ProviderBase provider,
+    Object? previousValue,
+    Object? newValue,
+    ProviderContainer container,
+  ) {
+    debugPrint('''
+{
+  "provider": "${provider.name ?? provider.runtimeType}",
+  "newValue": "$newValue"
+}''');
+  }
+}
