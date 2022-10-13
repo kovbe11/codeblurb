7040f6c82d02ba79f8693eec542d9b7288db22bd
kovacs.bence <kovacs.bence@autsoft.hu>
2022-10-13 18:59:30 +0200

    add query to ContentBundleRepository

--- backend/codeblurb-backend/src/main/java/hu/codeblurb/backend/service/ContentService.java
+++ backend/codeblurb-backend/src/main/java/hu/codeblurb/backend/service/ContentService.java
@@ -2,10 +2,11 @@ package hu.codeblurb.backend.service;
 
 import hu.codeblurb.backend.controller.dto.content.QuizSolutionRequest;
 import hu.codeblurb.backend.domain.content.CodingContent;
-import hu.codeblurb.backend.domain.content.Content;
 import hu.codeblurb.backend.domain.content.ContentBundle;
+import hu.codeblurb.backend.domain.content.QuizContent;
+import hu.codeblurb.backend.repository.CodingRepository;
 import hu.codeblurb.backend.repository.ContentBundleRepository;
-import hu.codeblurb.backend.repository.ContentRepository;
+import hu.codeblurb.backend.repository.QuizRepository;
 import hu.codeblurb.backend.security.exception.InconsistentDatabaseException;
 import hu.codeblurb.backend.security.service.AuthenticationFacade;
 import hu.codeblurb.backend.service.dto.ContentBundleResult;
@@ -21,36 +22,40 @@ import java.util.List;
 public class ContentService {
 
     private final AuthenticationFacade authenticationFacade;
-    private final ContentRepository contentRepository;
+    private final CodingRepository codingRepository;
+    private final QuizRepository quizRepository;
     private final ContentBundleRepository contentBundleRepository;
     private final CodeRunnerService codeRunnerService;
+    private final QuizSolutionCheckerService quizSolutionCheckerService;
     private final Mapper mapper;
 
     public List<ContentBundleResult> getPurchasedContentBundles() {
         return authenticationFacade.getCurrentCustomerId()
                 .map(this::getPurchasedContentBundles)
-                .orElseGet(List::of)
-                .stream()
-                .map(it -> new ContentBundleResult()) // TODO
-                .toList();
+                .map(mapper::mapContentBundles)
+                .orElseThrow(() -> new InconsistentDatabaseException("Authenticated request does not have customer id!")); //should not happen
     }
 
     //TODO: CACHE
     private List<ContentBundle> getPurchasedContentBundles(Integer customerId) {
-        return List.of();
+        return contentBundleRepository.findContentBundlesPurchasedByCustomerId(customerId);
     }
 
 
     public void runCodeSolutionFor(Integer contentId, String code) {
-        contentRepository.findById(contentId)
-                .filter(it -> it.getContentType() == Content.ContentType.CODING && ((CodingContent) it).getCodingContentType() == CodingContent.CodingContentType.SCRATCH)
-                .orElseThrow(() -> new EntityNotFoundException("todo"));
+        final var coding = codingRepository.findById(contentId)
+                .filter(it -> it.getCodingContentType() == CodingContent.CodingContentType.SCRATCH)
+                .orElseThrow(() -> new EntityNotFoundException(CodingContent.class, contentId));
 
-        final var result = codeRunnerService.runAndGetOutputFor(code);
+        codeRunnerService.runAndCheckOutputFor(coding);
         //TODO
     }
 
     public void checkSolutionForQuiz(Integer contentId, QuizSolutionRequest quizSolutionRequest) {
+        final var quiz = quizRepository.findById(contentId)
+                .orElseThrow(() -> new EntityNotFoundException(QuizContent.class, contentId));
 
+        quizSolutionCheckerService.checkSolution(quiz, quizSolutionRequest);
+        //TODO
     }
 }
