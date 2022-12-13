8765911c41c3e6c935db580d48689240fdcafa52
Levente Czeglédi <levente.czegledi@kadasolutions.ch>
2022-12-13 19:29:28 +0100

    finish results tab ui

--- mobile/codeblurb/lib/core/themes/app_theme.dart
+++ mobile/codeblurb/lib/core/themes/app_theme.dart
@@ -7,11 +7,23 @@ part "input_decoration_theme.dart";
 abstract class AppTheme {
   static get light => ThemeData(inputDecorationTheme: _kInputDecorationTheme);
   static get dark => ThemeData.dark().copyWith(
-      inputDecorationTheme: _kInputDecorationTheme,
-      appBarTheme: const AppBarTheme(
-        color: AppColors.baseDark,
-        elevation: 15,
-      ),
-      scaffoldBackgroundColor: AppColors.baseDark);
+        inputDecorationTheme: _kInputDecorationTheme,
+        appBarTheme: const AppBarTheme(
+          color: AppColors.baseDark,
+          elevation: 15,
+        ),
+        scaffoldBackgroundColor: AppColors.baseDark,
+        dividerColor: Colors.transparent,
+        expansionTileTheme: const ExpansionTileThemeData(
+          childrenPadding: EdgeInsets.zero,
+          tilePadding: EdgeInsets.symmetric(horizontal: 15),
+          collapsedIconColor: AppColors.baseLightGrey,
+          expandedAlignment: Alignment.topCenter,
+          iconColor: null,
+          backgroundColor: AppColors.baseDarkGrey,
+          collapsedBackgroundColor: AppColors.baseDarkGrey,
+        ),
+      );
+
   //up for later customization
 }
